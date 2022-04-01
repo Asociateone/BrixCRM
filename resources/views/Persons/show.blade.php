c3837fe8281351ddc961fb8176a58b126cd3cc7f
Rainaldo <61870393+Asociateone@users.noreply.github.com>
2022-04-01 23:44:37 +0200

    made the show page more readebol

--- resources/views/Persons/show.blade.php
+++ resources/views/Persons/show.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
     <div class="container">
-        {{$person}}
+        <table>
+            <thead>
+                <td>First name</td>
+                <td>Last name</td>
+                <td>Blocked</td>
+            </thead>
+            <tbody>
+                <td>
+                    {{$person->first_name}}
+                </td>
+                <td>
+                    {{$person->last_name}}
+                </td>
+                <td>
+                    @if ($person->actions_blocked)
+                    Blocked
+                    @else
+                    Not Blocked
+                    @endif
+                </td>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="container">
+
     </div>
 @endsection
