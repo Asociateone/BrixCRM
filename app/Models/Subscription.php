6d4bb99a96705e4bba8d0a44d6abd315cf4705b4
Rainaldo <61870393+Asociateone@users.noreply.github.com>
2022-04-03 22:09:41 +0200

    has many relation

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subscription extends Model
 {
@@ -19,5 +20,10 @@ class Subscription extends Model
     public function person(): BelongsTo
     {
         return $this->belongsTo(Person::class, 'id', 'person_id');
+    }
+
+    public function Invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
     }
 }
